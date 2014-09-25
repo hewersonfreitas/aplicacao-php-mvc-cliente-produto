6ad12ca4bfe3df01c28835788dc60c5bd61b03e1
Hewerson Freitas <hewerson.freitas@gmail.com>
2014-09-24 22:10:02 -0300

    Adicionados metodos, getAll, getId, update, delete no DAO de Usuario;

--- dao/UsuarioDao.class.php
+++ dao/UsuarioDao.class.php
@@ -23,12 +23,37 @@ class UsuarioDao {
         $statement->execute();
     }
     
-    public function getAll($param) {
+    public function getAll() {
+        $sql = "SELECT * FROM usuario";
+        return $resultSet = $this->conn->Conectar()->query($sql)->fetch(PDO::FETCH_OBJ);
+       // return $resultSet->execute();
         
     }
     
-    public function getId($param) {
-        
+    public function getId($id) {
+        $sql = "SELECT * FROM usuario WHERE id_pessoa = ".$id;
+        $resultSet = $this->conn->Conectar()->query($sql);
+        $resultSet->execute();
+        return $resultSet->fetch(PDO::FETCH_OBJ);
+    }
+    
+    public function update(Usuario $user) {
+
+        $sql = "UPDATE usuario SET nome_usuario=:nome,email_usuario=:email,senha_usuario = :senha,data_nasc_usuario=:data_nascimento)";
+        $statement = $this->conn->Conectar()->prepare($sql);
+        $statement->bindValue(":nome", $user->getNomeUsuario());
+        $statement->bindValue(":email", $user->getEmailUsuario());
+        $statement->bindValue(":senha", $user->getSenhaUsuario());
+        $statement->bindValue(":data_nascimento", $user->getDataNascimentoUsuario());
+        $statement->execute();
+    }
+    
+    public function delete($id) {
+        $sql = "SELECT * FROM usuario WHERE id_pessoa = :id";
+        $resultSet = $this->conn->Conectar()->query($sql);
+        $resultSet->bindValue("id", $id);
+        $resultSet->execute();
+        return $resultSet->fetch(PDO::FETCH_OBJ);;
     }
 
 }
